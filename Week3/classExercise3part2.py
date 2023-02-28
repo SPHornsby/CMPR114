5e8f76801f5b8346268ea126e2b38ae7bb981ee0
SPHornsby <sean.hornsby@gmail.com>
2023-02-27 19:57:42 -0800

    Add product_over_100 func

--- Week3/classExercise3part2.py
+++ Week3/classExercise3part2.py
@@ -1,19 +1,29 @@
 # This program calculates the sum and average
 # of a series of numbers entered by the user.
+def sum_avg():
+    MAX = 5 # Maximum number of numbers
 
-MAX = 5 # Maximum number of numbers
+    # Initialize an accumulator variable
+    total = 0.0
 
-# Initialize an accumulator variable
-total = 0.0
+    # Explain to the user
+    print('This program calculates the sum and average of')
+    print(f'the {MAX} numbers you will enter.')
 
-# Explain to the user
-print('This program calculates the sum and average of')
-print(f'{MAX} numbers you will enter.')
+    # Get the numbers and accumulate them
+    for counter in range(MAX):
+        number = int(input('Enter a number: '))
+        total += number
 
-# Get the numbers and accumulate them
-for counter in range(MAX):
-    number = int(input('Enter a number: '))
-    total += number
+    # Display sum and average
+    print(f'The total is {total} and the average is {total/MAX}')
 
-# Display sum and average
-print(f'The total is {total} and the average is {total/MAX}')
+# This program will take user input, multiply it by 10 and continue looping until that product is greater than or equal to 100
+def product_over_100():
+    product = 0
+    while product < 100:
+        product = 10 * int(input('Enter a number: '))
+        print(f'Product is {product}')
+    print('You have escaped the loop!')
+# product_over_100()   
+
